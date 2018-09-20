54e38144f12cf3c3c72ce0c91fcc6cd43a6f3920
Ethan Lee <hello@ethanl.ee>
2018-09-20 00:33:54 -0700

    Minor command line changes

--- bin/whales.js
+++ bin/whales.js
@@ -5,6 +5,7 @@ const program = require("commander");
 program
   .version("0.1.0")
   .command("server", "start a Whales server for the project in this directory")
+    .alias("s")
   .command("new", "create a new Whales project in this directory")
   .command("bootstrap", "rebuild the Docker image used for this project")
   .command("setup", "(macOS only) install tool dependencies - this gets run automatically")
