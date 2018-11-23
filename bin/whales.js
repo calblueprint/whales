9964e1c38de0e3195a7f1a40675d485875cf186d
Ethan Lee <hello@ethanl.ee>
2018-11-23 10:34:03 -0800

    Publish 0.2.3

--- bin/whales.js
+++ bin/whales.js
@@ -3,7 +3,7 @@
 const program = require("commander");
 
 program
-  .version("0.2.2")
+  .version("0.2.3")
   .command("server", "start a Whales server for the project in this directory")
     .alias("s")
   .command("new", "create a new Whales project in this directory")
